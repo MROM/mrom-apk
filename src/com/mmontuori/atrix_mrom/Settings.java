0f6f1208b1f1e6e85b0a423e55a065e35589fbb9
ravilov <ravilov@gmail.com>
2012-10-05 17:11:28 +0200

    added the settings screen

---
+++ src/com/mmontuori/atrix_mrom/Settings.java
@@ -0,0 +1,43 @@
+package com.mmontuori.atrix_mrom;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+
+public class Settings extends PreferenceActivity {
+	public boolean checkOnBoot;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.prefs);
+		loadPrefs(this);
+	}
+
+	public void loadPrefs(Context ctx) {
+		SharedPreferences prefs = null;
+		PreferenceScreen screen = null;
+		try {
+			screen = getPreferenceScreen();
+		}
+		catch (Exception ex) {
+			screen = null;
+		}
+		if (screen == null) {
+			prefs = PreferenceManager.getDefaultSharedPreferences(ctx);
+		} else {
+			prefs = screen.getSharedPreferences();	
+		}
+		if (prefs != null) {
+			checkOnBoot = prefs.getBoolean("check_on_boot", true);
+		} else {
+			checkOnBoot = true;
+		}
+	}
+
+	public static void savePrefs() {
+	}
+}
